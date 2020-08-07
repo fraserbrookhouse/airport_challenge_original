170b8cc8859db46e3ee2895849fa62f0dc0a4ca4
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-07 11:21:03 +0100

    made test for Aiport class to respond to method 'land', created method 'land' for class Airport,  passed test

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -4,4 +4,7 @@ describe Airport do
   it 'should exist' do
     expect(subject).to be_instance_of Airport
   end
+  it 'responds to method land' do
+    expect(subject).to respond_to(:land)
+  end
 end
