856be1f72ec729686ecada1c4b6bf87f22a2b8ea
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-09 15:37:46 +0100

    added failing test for error message if plane tries to land when airport is full

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -26,6 +26,10 @@ describe Airport do
       subject.land(plane)
       expect(subject.landed_planes).to include(plane)
     end
+    it 'throws as error when plane tries to land if airport is full' do
+      20.times { subject.land Plane.new }
+      expect { subject.land Plane.new }.to raise_error 'Airport full, no space to land'
+    end
   end
   describe '#take_off' do
     it 'removes plane from landed_planes array' do
